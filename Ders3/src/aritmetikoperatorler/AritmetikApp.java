5ede7103c6aff52810e6f057ecd0249fd255a492
yucel.kilicer <yucelkilicer@gmail.com>
2019-02-02 10:45:55 +0300

    İlk Branch

---
+++ Ders3/src/aritmetikoperatorler/AritmetikApp.java
@@ -0,0 +1,8 @@
+package aritmetikoperatorler;
+
+public class AritmetikApp {
+        public static void main(String[] args){
+                System.out.println("Merhaba İlk Branch");
+
+        }
+}
